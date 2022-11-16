828bcc2826a4babd87c41eae0af608a3674b6da7
yeonHo0817 <86468429+yeonho0817@users.noreply.github.com>
2022-11-16 13:45:59 +0900

    Delete backend/matching_mate(back) directory

--- backend/matching_mate(back)/src/main/java/com/matching/system/customRepository/MatchingPostCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matching.system.customRepository;
-
-import java.util.Date;
-
-public interface MatchingPostCustomRepository {
-    Date getMatchingDate(Long chattingRoomId);
-
-    Integer getJoinChatNumber(Long matchingPostId);
-}
--- backend/matching_mate(back)/src/main/java/com/matching/system/jwt/redis/RefreshTokenRedisRepository.java
+++
@@ -1,6 +0,0 @@
-package com.matching.system.jwt.redis;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
-}
